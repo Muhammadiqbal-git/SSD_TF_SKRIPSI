f67c223b3211178b3747add6b9ff62b5cf2c35e9
Iqbal <Iniiqbal21@gmail.com>
2023-07-22 11:38:30 +0700

    add custom path parameter in get_model_path() fn

--- Workspace/code/utils/io_utils.py
+++ Workspace/code/utils/io_utils.py
@@ -14,15 +14,20 @@ def get_log_path(model_type, custom_postfix=""):
     """
     return "logs/{}{}/{}".format(model_type, custom_postfix, datetime.now().strftime("%Y%m%d-%H%M%S"))
 
-def get_model_path(model_type):
+def get_model_path(model_type, custom_path=None):
     """Generating model path from model_type value for save/load model weights.
     inputs:
-        model_type = "vgg16", "mobilenet_v2"
+        model_type (str): "vgg16"
+        custom_path(str, optional): custom path for save/load model weights. Default to "../trained_ssd"
 
     outputs:
-        model_path = os model path, for example: "trained/ssd_vgg16_model_weights.h5"
+        model_path = os model path, for example: "../trained/ssd_vgg16_model_weights.h5"
     """
-    main_path = "trained"
+    rel_path = os.path.relpath(os.path.join(os.getcwd(), os.pardir))
+    main_path = os.path.join(rel_path, "trained_ssd")
+    if custom_path:
+        main_path = custom_path
+
     if not os.path.exists(main_path):
         os.makedirs(main_path)
     model_path = os.path.join(main_path, "ssd_{}_model_weights.h5".format(model_type))
@@ -37,8 +42,8 @@ def handle_args():
     parser = argparse.ArgumentParser(description="SSD: Single Shot MultiBox Detector Implementation")
     parser.add_argument("-handle-gpu", action="store_true", help="Tensorflow 2 GPU compatibility flag")
     parser.add_argument("--backbone", required=False,
-                        default="mobilenet_v2",
-                        metavar="['mobilenet_v2', 'vgg16']",
+                        default="vgg16",
+                        metavar="['vgg16']",
                         help="Which backbone used for the ssd")
     args = parser.parse_args()
     return args
